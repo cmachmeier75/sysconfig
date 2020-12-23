6b587466ed57578e1ca42a05dbe63d55aa36f13f
Christian Machmeier <christian.machmeier@gmail.com>
2020-12-23 19:35:40 +0100

    added ssh-configuration

--- configure_base.sh
+++ configure_base.sh
@@ -1,19 +1,52 @@
 #!/bin/bash
 
-echo ""
-openssl
-openssh-server
+echo "Configuring openssh-'server'.."
+sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.factory-defaults
+sudo chmod a-w /etc/ssh/sshd_config.factory-defaults
+read -r -d '' sshd_config <<'EOF'
+PermitRootLogin no
+MaxAuthTries 3
+PubkeyAuthentication yes
+AuthorizedKeysFile .ssh/authorized_keys
+PasswordAuthentication no
+PermitEmptyPasswords no
+ChallengeResponseAuthentication no
+UsePAM no
+AllowAgentForwarding yes
+AllowTcpForwarding no
+X11Forwarding yes
+PrintMotd no
+TCPKeepAlive yes
+MaxStartups 3
+AcceptEnv LANG LC_*
+Subsystem sftp /usr/lib/openssh/sftp-server
+AllowUsers ubuntu 
 
-echo "Updating firewall rules (SSH)"
+EOF
+
+sudo echo "$sshd_config" > ./sshd_config_test
+sudo systemctl restart ssh
+
+echo "Creating personal SSH-key.."
+ssh-keygen -t rsa -b 4096
+
+cat ~/.ssh/id_rsa
+
+echo "Adding your public SSH-key to 'authorized_keys'"
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+
+echo "Updating firewall rules (SSH).."
 sudo ufw allow ssh
 sudo ufw enable
 sudo ufw status
 
 echo "Configuring AWS"
-aws configure --profile cm
+# aws configure --profile cm
 
+echo "Refreshing font-cache.."
+fc-cache -fv
 
-echo "Enabling Gnome Shell extensions"
+echo "Enabling Gnome Shell extensions.."
 gsettings set org.gnome.shell disable-user-extensions false
 gnome-extensions enable dash-to-panel@jderose9.github.com
 gnome-extensions enable no-title-bar@jonaspoehler.de
