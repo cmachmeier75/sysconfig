f165c29f673db6748d105370ee46872d36204919
Christian Machmeier <christian.machmeier@gmail.com>
2020-12-30 14:58:39 +0100

    added ranger qt5ct beekeeper to apps-installs

--- 30_install_apps.sh
+++ 30_install_apps.sh
@@ -2,7 +2,7 @@
 
 # OPTIONAL PACKAGES
 echo "Installing OPTIONAL PACKAGES.."
-packages=( awscli chromium-browser gnome-tweaks lxappearance mc newsboat synaptic tmux vim )
+packages=( awscli chromium-browser gnome-tweaks lxappearance mc newsboat ranger qt5ct synaptic tmux vim )
 for p in "${packages[@]}"; do
     echo " > $p";
     sudo apt-get install $p -y;
@@ -10,8 +10,12 @@ done
 
 # CUSTOM PACKAGES
 echo "Adding CUSTOM PACKAGE REPOS.."
+echo " > BeeKeeper"
+curl -fsSL https://bintray.com/user/downloadSubjectPublicKey?username=bintray | gpg --dearmor | sudo dd of=/etc/apt/trusted.gpg.d/beekeeper.gpg
+yes "" | sudo add-apt-repository "deb [arch=amd64] https://dl.bintray.com/beekeeper-studio/releases disco main"
+
 echo " > Docker"
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor | sudo dd of=/etc/apt/trusted.gpg.d/docker.gpg
 yes "" | sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 echo " > Ghostwriter"
@@ -24,29 +28,32 @@ echo " > KeePassXC"
 yes "" | sudo add-apt-repository ppa:phoerious/keepassxc
 
 echo " > OpenVPN"
-curl -fsSL https://swupdate.openvpn.net/repos/repo-public.gpg| sudo apt-key add -
-yes "" | sudo add-apt-repository "deb http://build.openvpn.net/debian/openvpn/stable focal main"
+curl -fsSL https://swupdate.openvpn.net/repos/repo-public.gpg | gpg --dearmor | sudo dd of=/etc/apt/trusted.gpg.d/openvpn.gpg
+yes "" | sudo add-apt-repository "deb [arch=amd64] http://build.openvpn.net/debian/openvpn/stable focal main"
 
 echo " > Signal"
-curl -fsSL https://updates.signal.org/desktop/apt/keys.asc | sudo apt-key add -
+curl -fsSL https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor | sudo dd of=/etc/apt/trusted.gpg.d/signal.gpg
 yes "" | sudo add-apt-repository "deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main"
 
 echo " > Spotify"
-curl -fsSL https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - 
-yes "" | sudo add-apt-repository "deb http://repository.spotify.com stable non-free"
+curl -fsSL https://download.spotify.com/debian/pubkey_0D811D58.gpg | gpg --dearmor | sudo dd of=/etc/apt/trusted.gpg.d/spotify.gpg 
+yes "" | sudo add-apt-repository "deb [arch=amd64] http://repository.spotify.com stable non-free"
 
 echo " > Syncthing"
-curl -fsSL https://syncthing.net/release-key.txt | sudo apt-key add -
-yes "" | sudo add-apt-repository "deb https://apt.syncthing.net/ syncthing stable"
+curl -fsSL https://syncthing.net/release-key.txt | gpg --dearmor | sudo dd of=/etc/apt/trusted.gpg.d/syncthing.gpg
+yes "" | sudo add-apt-repository "deb [arch=amd64] https://apt.syncthing.net/ syncthing stable"
 
 echo " > VSCodium"
-curl -fsSL https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | gpg --dearmor | sudo dd of=/etc/apt/trusted.gpg.d/vscodium.gpg 
-yes "" | sudo add-apt-repository "deb https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/debs/ vscodium main"
+curl -fsSL https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | gpg --dearmor | sudo dd of=/etc/apt/trusted.gpg.d/vscodium.gpg
+yes "" | sudo add-apt-repository "deb [arch=amd64] https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/debs/ vscodium main"
 
 echo "Refreshing list of packages.."
 sudo apt-get update -y
 
 echo "Installing CUSTOM PACKAGES.."
+echo " > BeeKeeper"
+sudo apt-get install beekeeper-studio -y
+
 echo " > Docker"
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 
